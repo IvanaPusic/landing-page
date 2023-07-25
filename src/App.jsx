2063da6ab96ac1858e17f7b2931c99f6017673a5
Ivana Pusic <pusicka97@gmail.com>
2023-07-25 13:52:02 +0200

    setup application

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Container } from 'react-bootstrap';
+function App() {
+  return (
+    <Container className=' min-vh-100 d-flex justify-content-center align-items-center '>
+      <h1 className='text-xl'> Near Balkans</h1>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Certifications from './Certifications';
+import Contacts from './Contacts';
+import Error from './Error';
+import Events from './Events';
+import Grants from './Grants';
+import Home from './Home';
+import News from './News';
+
+export { Certifications, Contacts, Error, Events, Grants, Home, News };
